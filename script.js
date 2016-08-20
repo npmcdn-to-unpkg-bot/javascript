e29beb6a8898fd66daac14721fd544c92de8255b
clamonsira <clara_simon@gmx.de>
2016-08-20 15:44:12 +0200

    1. version

---
+++ script.js
@@ -0,0 +1,28 @@
+ 
+ 
+// var eingabe = prompt("Bitte gib eine Zahl ein.");
+ 
+ //alert("Du hast " + eingabe + " eingegeben.");
+
+ /*
+  * längerer Kommentar
+  * 
+  * */
+ 
+ //d3.select("body").transition().style("color", "white");
+ 
+ d3.select("body").style("background-color", "orange");
+ 
+ var x=5, y;
+ 
+ x *= 2; // x=2*x
+ 
+ y = Math.sqrt(2);
+ 
+ if (eingabe > 10) {
+        x++;
+     
+ } else {
+        x--;
+        alert(x);
+ }
